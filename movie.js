60e57dff74b84577d3598ae3f4ad0c08adb36961
ko-ko-naing <kokonaingk723@gmail.com>
2021-11-21 16:12:56 +0630

    awsV1

---
+++ movie.js
@@ -0,0 +1,12 @@
+               
+function myFunction() {
+    var navList = document.querySelector(".nav-right");
+    navList.classList.toggle("show-nav");
+ 
+    var btn = document.querySelector(".mobile-menu-btn");
+    console.log("hello button!");
+ 
+    var icon = document.querySelector(".fa");
+    icon.classList.toggle("fa-times");
+ }
+ 
